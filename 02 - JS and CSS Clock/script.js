5fd2c04f6fe6fb5972baa882202e64ec3212c567
Nour Ibrahim <nournoon12@gmail.com>
2025-11-13 14:51:38 +0200

    Just trying to do the second day script again.

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,21 +1,22 @@
-const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
-  const hours = now.getHours();
+const secondsHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
 
+function clockTiming() {
+  const currentDate = new Date();
+
+  const seconds = currentDate.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
+  secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+  const minutes = currentDate.getMinutes();
   const minutesDegree = (minutes / 60) * 360 + 90;
+  minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  const hours = currentDate.getHours();
   const hoursDegree = (hours / 60) * 360 + 90;
-
-  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
-  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
-
+  hoursHand.style.transform = `rotate(${hoursDegree}deg)`;
   console.log(seconds);
 }
 
-setInterval(setDate, 1000);
+setInterval(clockTiming, 1000);
