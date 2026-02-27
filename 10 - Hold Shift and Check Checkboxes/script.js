6ac6c81f115f2a268d863eb84078aa15af01c9d8
Nour Ibrahim <nournoon12@gmail.com>
2026-02-27 15:11:10 +0200

    checkbox challenge completed.

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,26 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(event) {
+  let inBetween = false;
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log(inBetween);
+        checkbox.style.accentColor = "yellow";
+        console.log("inBetween start and end .");
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck),
+);
